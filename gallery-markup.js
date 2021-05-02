617cfa648a67c7c4cf86aace0bd87df8bccf6513
Oksana Martynenko <oks.martynenko@gmail.com>
2021-05-02 18:54:22 +0300

    Создает и рендерит разметку по массиву данных и предоставленному шаблону

--- gallery-markup.js
+++ gallery-markup.js
@@ -1,2 +1,27 @@
-import gallery from './gallery-items.js';
-console.log(gallery);
+import images from './gallery-items.js';
+
+const galleryContainer = document.querySelector('.js-gallery');
+const galleryMarkup = createGalleryMarkup(images);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(images) {
+  return images.map(({preview, original, description}) => {
+    return `
+    <li class="gallery__item">
+      <a
+        class="gallery__link"
+        href="${original}"
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li >
+    `;
+    })
+    .join(''); 
+};
